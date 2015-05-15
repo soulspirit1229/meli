3883e2b85fbb7a65b3ed5a9f052fceb39415df6a
soulspirit1229 <huangheyuan1229@126.com>
2015-05-16 01:29:02 +0800

    init commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,47 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, :alert => '你没有权限访问该页面'
+  end
+
+  def render_success(msg = nil, data = {})
+    render :json => {
+      success: true,
+      message: msg.to_s
+    }.merge(data)
+  end
+
+  def render_fail(msg = nil, model = nil)
+    res = {
+      success: false,
+      message: msg.to_s,
+    }
+
+    if model
+      if model.kind_of?(Hash)
+        res.merge!(model)
+      else
+        res.merge!( errors: flatten_errors(model.errors.messages) )
+      end
+    end
+
+    render :json => res
+  end
+
+  protected
+  def flatten_errors(errors)
+    errors.inject({}) { |res, (k,v)| res[k] = v.first; res }
+  end
+
+  def valid_on(model, column_name)
+    if model.valid?
+      return false
+    else
+      model.errors[column_name].first
+    end
+  end
+
+end
